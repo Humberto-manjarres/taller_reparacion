a8b6f7c88733e4d40d37bc97a842e74671474957
humberto <totopercusion@gmail.com>
2025-04-13 11:27:54 -0500

    init commit

---
+++ src/main/java/com/taller/TallerReparacionApplication.java
@@ -0,0 +1,13 @@
+package com.taller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerReparacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerReparacionApplication.class, args);
+	}
+
+}
